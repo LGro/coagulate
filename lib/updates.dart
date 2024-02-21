53777b6665799bff857c2e4e9d6d18f3648d5187
Lukas Grossberger <code@grossberger.xyz>
2024-02-21 20:57:48 +0100

    explore updates layout

--- lib/updates.dart
+++ lib/updates.dart
@@ -4,12 +4,19 @@ import 'package:flutter/material.dart';
 
 class UpdatesPage extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => ListView(
-        children: List.generate(
-          10,
-          (index) => ListTile(
-            title: Text('Update $index'),
-          ),
+  Widget build(BuildContext context) => ListView(children: const [
+        ListTile(
+          title: Text('Ronja Dudeli                                    (2d)\n'
+              'Name: Timo → Ronja'),
         ),
-      );
+        ListTile(
+          title: Text('Timo Dudeli                                    (1m)\n'
+              'Home: Heimsheimer St... → Burgstraße...'),
+        ),
+        ListTile(
+          title:
+              Text('Helli Schmudela                                    (2y)\n'
+                  'Work: +3011311411 → +2144242200'),
+        ),
+      ]);
 }
