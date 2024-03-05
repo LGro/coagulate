611a955fab88eb115b018273fc8182905e19c0f1
Lukas Grossberger <code@grossberger.xyz>
2024-03-05 16:36:20 +0100

    set initial map view depending on markers

--- lib/map.dart
+++ lib/map.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:math';
 
+import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_map/flutter_map.dart';
@@ -23,91 +24,100 @@ class MapPage extends StatelessWidget {
   Widget build(BuildContext context) => BlocProvider(
       create: (context) => CoagContactCubit()..refreshContactsFromSystem(),
       child: BlocConsumer<CoagContactCubit, CoagContactState>(
-        listener: (context, state) async {},
-        builder: (context, state) => FlutterMap(
-          options: MapOptions(
-            // initialCenter: LatLng((maxLatLng.latitude + minLatLng.latitude) / 2,
-            //     (maxLatLng.longitude + minLatLng.longitude) / 2),
-            initialZoom: 6,
-            maxZoom: 15,
-          ),
-          children: <Widget>[
-            TileLayer(
-              urlTemplate: (const String.fromEnvironment(
-                          'COAGULATE_MAPBOX_PUBLIC_TOKEN')
-                      .isEmpty)
-                  ? 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
-                  // TODO: Add {r} along with retinaMode.isHighDensity and TileLayer.retinaMode
-                  : 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}?access_token=${const String.fromEnvironment('COAGULATE_MAPBOX_PUBLIC_TOKEN')}',
-            ),
-            MarkerClusterLayerWidget(
-                options: MarkerClusterLayerOptions(
-                    maxClusterRadius: 100,
-                    size: const Size(40, 40),
-                    alignment: Alignment.center,
-                    padding: const EdgeInsets.all(50),
-                    maxZoom: 15,
-                    builder: (context, markers) => DecoratedBox(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                            color: Colors.deepPurple),
-                        child: Center(
-                            child: Text(markers.length.toString(),
-                                style: const TextStyle(color: Colors.white)))),
-                    // TODO: Bring back to filter out the contacts with actual coordinate infos
-                    // .where((contact) => contact.lng != null && contact.lat != null)
-                    markers: state.contacts.values
-                        .map((contact) => Marker(
-                            height: 60,
-                            width: 100,
-                            point: _contactToLatLng(contact),
-                            alignment: Alignment.topCenter,
-                            child: GestureDetector(
-                                onTap: () {
-                                  unawaited(Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (_) => ContactPage(
-                                              contactId: contact.contact.id))));
-                                },
-                                child: Column(
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: [
-                                      Text(
-                                        '${contact.contact.name.first} ${contact.contact.name.last}',
-                                        overflow: TextOverflow.ellipsis,
-                                        style: const TextStyle(fontSize: 14),
-                                      ),
-                                      // TODO: Display label of the address
-                                      const Text(
-                                        '(label)',
-                                        overflow: TextOverflow.ellipsis,
-                                        style: TextStyle(fontSize: 10),
-                                      ),
-                                      const SizedBox(width: 5),
-                                      const Icon(Icons.location_pin,
-                                          size: 26, color: Colors.deepPurple)
-                                    ]))))
-                        .toList())),
-            RichAttributionWidget(
-                showFlutterMapAttribution: false,
-                attributions: [
-                  if (const String.fromEnvironment(
-                          'COAGULATE_MAPBOX_PUBLIC_TOKEN')
-                      .isEmpty)
-                    TextSourceAttribution(
-                      'OpenStreetMap',
-                      onTap: () async => launchUrl(
-                          Uri.parse('https://www.openstreetmap.org/copyright')),
-                    )
-                  else
-                    TextSourceAttribution(
-                      'Mapbox',
-                      onTap: () async => launchUrl(
-                          Uri.parse('https://www.mapbox.com/about/maps/')),
-                    )
-                ])
-          ],
-        ),
-      ));
+          listener: (context, state) async {},
+          builder: (context, state) {
+            const mapboxToken =
+                String.fromEnvironment('COAGULATE_MAPBOX_PUBLIC_TOKEN');
+
+            final markers = state.contacts.values
+                // TODO: Bring back to filter out the contacts with actual coordinate infos
+                // .where((contact) => contact.lng != null && contact.lat != null)
+                .map((contact) => Marker(
+                    height: 60,
+                    width: 100,
+                    point: _contactToLatLng(contact),
+                    alignment: Alignment.topCenter,
+                    child: GestureDetector(
+                        onTap: () {
+                          unawaited(Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (_) => ContactPage(
+                                      contactId: contact.contact.id))));
+                        },
+                        child:
+                            Column(mainAxisSize: MainAxisSize.min, children: [
+                          Text(
+                            '${contact.contact.name.first} ${contact.contact.name.last}',
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(fontSize: 14),
+                          ),
+                          // TODO: Display label of the address
+                          const Text(
+                            '(label)',
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(fontSize: 10),
+                          ),
+                          const SizedBox(width: 5),
+                          const Icon(Icons.location_pin,
+                              size: 26, color: Colors.deepPurple)
+                        ]))))
+                .toList();
+
+            final allLats = markers.map((m) => m.point.latitude).asList();
+            final allLons = markers.map((m) => m.point.longitude).asList();
+            allLats.sort();
+            allLons.sort();
+
+            return FlutterMap(
+              options: MapOptions(
+                initialCenter: LatLng(
+                  (allLats.last + allLats.first) / 2,
+                  (allLons.last + allLons.first) / 2,
+                ),
+                initialZoom: 3,
+                maxZoom: 15,
+              ),
+              children: <Widget>[
+                TileLayer(
+                  urlTemplate: (mapboxToken.isEmpty)
+                      ? 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+                      // TODO: Add {r} along with retinaMode.isHighDensity and TileLayer.retinaMode
+                      : 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}?access_token=${mapboxToken}',
+                ),
+                MarkerClusterLayerWidget(
+                    options: MarkerClusterLayerOptions(
+                  maxClusterRadius: 100,
+                  size: const Size(40, 40),
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(50),
+                  maxZoom: 15,
+                  markers: markers,
+                  builder: (context, markers) => DecoratedBox(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.deepPurple),
+                      child: Center(
+                          child: Text(markers.length.toString(),
+                              style: const TextStyle(color: Colors.white)))),
+                )),
+                RichAttributionWidget(
+                    showFlutterMapAttribution: false,
+                    attributions: [
+                      if (mapboxToken.isEmpty)
+                        TextSourceAttribution(
+                          'OpenStreetMap',
+                          onTap: () async => launchUrl(Uri.parse(
+                              'https://www.openstreetmap.org/copyright')),
+                        )
+                      else
+                        TextSourceAttribution(
+                          'Mapbox',
+                          onTap: () async => launchUrl(
+                              Uri.parse('https://www.mapbox.com/about/maps/')),
+                        )
+                    ])
+              ],
+            );
+          }));
 }
