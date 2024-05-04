f550baa8aec4fe744d852771b4bc9b5d6eb22970
Lukas Grossberger <code@grossberger.xyz>
2024-05-04 16:16:08 +0200

    update mocked providers, add logging

--- test/mocked_providers.dart
+++ test/mocked_providers.dart
@@ -17,73 +17,97 @@ class DummyPersistentStorage extends PersistentStorage {
 
   Map<String, CoagContact> contacts;
   String? profileContactId;
+  List<String> log = [];
 
   @override
   Future<void> addUpdate(ContactUpdate update) {
+    log.add('addUpdate');
     // TODO: implement addUpdate
     throw UnimplementedError();
   }
 
   @override
-  Future<Map<String, CoagContact>> getAllContacts() => Future.value(contacts);
+  Future<Map<String, CoagContact>> getAllContacts() async {
+    log.add('getAllContacts');
+    return Future.value(contacts);
+  }
 
   @override
-  Future<CoagContact> getContact(String coagContactId) =>
-      Future.value(contacts[coagContactId]);
+  Future<CoagContact> getContact(String coagContactId) async {
+    log.add('getContact:$coagContactId');
+    return Future.value(contacts[coagContactId]);
+  }
 
   @override
-  Future<String?> getProfileContactId() => Future.value(profileContactId);
+  Future<String?> getProfileContactId() async {
+    log.add('getProfileContactId');
+    return Future.value(profileContactId);
+  }
 
   @override
-  Future<List<ContactUpdate>> getUpdates() => Future.value([]);
+  Future<List<ContactUpdate>> getUpdates() async {
+    log.add('getUpdates');
+    return Future.value([]);
+  }
 
   @override
   Future<void> removeContact(String coagContactId) async {
+    log.add('removeContact:$coagContactId');
     contacts.remove(coagContactId);
   }
 
   @override
   Future<void> setProfileContactId(String profileContactId) {
+    log.add('setProfileContactId:$profileContactId');
     // TODO: implement setProfileContactId
     throw UnimplementedError();
   }
 
   @override
   Future<void> updateContact(CoagContact contact) async {
+    log.add('updateContact:${contact.coagContactId}');
     contacts[contact.coagContactId] = contact;
   }
 }
 
 class DummyDistributedStorage extends DistributedStorage {
+  List<String> log = [];
+
   @override
   Future<(String, String)> createDHTRecord() {
+    log.add('createDHTRecord');
     // TODO: implement createDHTRecord
     throw UnimplementedError();
   }
 
   @override
   Future<bool> isUpToDateSharingDHT(CoagContact contact) {
+    log.add('isUpToDateSharingDHT:${contact.coagContactId}');
     // TODO: implement isUpToDateSharingDHT
     throw UnimplementedError();
   }
 
   @override
   Future<String> readPasswordEncryptedDHTRecord(
-          {required String recordKey, required String secret}) =>
-      Future.value(json.encode(removeNullOrEmptyValues(
-          filterAccordingToSharingProfile(CoagContact(
-                  coagContactId: '',
-                  systemContact: Contact(displayName: 'Contact From DHT')))
-              .toJson())));
+      {required String recordKey, required String secret}) async {
+    log.add('readPasswordEncryptedDHTRecord:$recordKey:$secret');
+    return Future.value(json.encode(removeNullOrEmptyValues(
+        filterAccordingToSharingProfile(CoagContact(
+                coagContactId: '',
+                systemContact: Contact(displayName: 'Contact From DHT')))
+            .toJson())));
+  }
 
   @override
   Future<CoagContact> updateContactReceivingDHT(CoagContact contact) {
+    log.add('updateContactReceivingDHT:${contact.coagContactId}');
     // TODO: implement updateContactReceivingDHT
     throw UnimplementedError();
   }
 
   @override
   Future<CoagContact> updateContactSharingDHT(CoagContact contact) {
+    log.add('updateContactSharingDHT:${contact.coagContactId}');
     // TODO: implement updateContactSharingDHT
     throw UnimplementedError();
   }
@@ -94,12 +118,14 @@ class DummyDistributedStorage extends DistributedStorage {
       required String recordWriter,
       required String secret,
       required String content}) {
+    log.add('updatePasswordEncryptedDHTRecord:$recordKey');
     // TODO: implement updatePasswordEncryptedDHTRecord
     throw UnimplementedError();
   }
 
   @override
   Future<void> watchDHTRecord(String key) {
+    log.add('watchDHTRecord:$key');
     // TODO: implement watchDHTRecord
     throw UnimplementedError();
   }
@@ -109,16 +135,23 @@ class DummySystemContacts extends SystemContactsBase {
   DummySystemContacts(this.contacts);
 
   List<Contact> contacts;
+  List<String> log = [];
 
   @override
-  Future<Contact> getContact(String id) =>
-      Future.value(contacts.where((c) => c.id == id).first);
+  Future<Contact> getContact(String id) async {
+    log.add('getContact:$id');
+    return Future.value(contacts.where((c) => c.id == id).first);
+  }
 
   @override
-  Future<List<Contact>> getContacts() => Future.value(contacts);
+  Future<List<Contact>> getContacts() async {
+    log.add('getContacts');
+    return Future.value(contacts);
+  }
 
   @override
   Future<Contact> updateContact(Contact contact) {
+    log.add('updateContact:${json.encode(contact.toJson())}');
     if (contacts.where((c) => c.id == contact.id).isNotEmpty) {
       contacts =
           contacts.map((c) => (c.id == contact.id) ? contact : c).asList();
@@ -126,5 +159,17 @@ class DummySystemContacts extends SystemContactsBase {
       contacts.add(contact);
     }
     return Future.value(contact);
+  }
+
+  @override
+  Future<Contact> insertContact(Contact contact) {
+    contacts.add(contact);
+    return Future.value(contact);
+  }
+
+  @override
+  Future<bool> requestPermission() {
+    // TODO: implement requestPermission
+    throw UnimplementedError();
   }
 }
