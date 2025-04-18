f1c7343bcf26bae7122e5fb3d898a1584c17b3c8
Lukas Grossberger <code@grossberger.xyz>
2025-04-18 17:49:48 +0200

    move test

--- test/ui/contact_details_test.dart
+++ test/ui/contact_details_test.dart
@@ -22,6 +22,19 @@ Future<Widget> createContactPage(
         )));
 
 void main() {
+  test('Test number of contacts a location is shared with', () {
+    final memberships = {
+      'contact1': ['circle2'],
+      'contact2': ['circle1', 'circle2']
+    };
+    expect(numberContactsShared([], []), 0);
+    expect(numberContactsShared([[]], []), 0);
+    expect(numberContactsShared(memberships.values, []), 0);
+    expect(numberContactsShared(memberships.values, ['circle1']), 1);
+    expect(numberContactsShared(memberships.values, ['circle3']), 0);
+    expect(numberContactsShared(memberships.values, ['circle2']), 2);
+    expect(numberContactsShared(memberships.values, ['circle1', 'circle2']), 2);
+  });
   group('Contact Details Page Widget Tests', () {
     // testWidgets('Circles update causes details page update', (tester) async {
     //   final contact = CoagContact(
