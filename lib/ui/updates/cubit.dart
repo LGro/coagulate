3d017d957f165447a0dd644101adf1fbf5e0c3e1
Lukas Grossberger <code@grossberger.xyz>
2025-07-04 16:56:22 +0200

    fix update selection to display in app

--- lib/ui/updates/cubit.dart
+++ lib/ui/updates/cubit.dart
@@ -1,4 +1,4 @@
-// Copyright 2024 The Coagulate Authors. All rights reserved.
+// Copyright 2024 - 2025 The Coagulate Authors. All rights reserved.
 // SPDX-License-Identifier: MPL-2.0
 
 import 'dart:async';
@@ -9,6 +9,7 @@ import 'package:json_annotation/json_annotation.dart';
 
 import '../../data/models/contact_update.dart';
 import '../../data/repositories/contacts.dart';
+import '../utils.dart';
 
 part 'state.dart';
 part 'cubit.g.dart';
@@ -18,9 +19,12 @@ class UpdatesCubit extends Cubit<UpdatesState> {
       : super(const UpdatesState(UpdatesStatus.initial)) {
     _updatesSubscription = contactsRepository.getUpdatesStream().listen((_) =>
         emit(UpdatesState(UpdatesStatus.success,
-            updates: contactsRepository.getContactUpdates().reversed)));
+            updates: contactsRepository.getContactUpdates().reversed.where(
+                (u) => contactUpdateSummary(u.oldContact, u.newContact)
+                    .isNotEmpty))));
     emit(UpdatesState(UpdatesStatus.success,
-        updates: contactsRepository.getContactUpdates().reversed));
+        updates: contactsRepository.getContactUpdates().reversed.where((u) =>
+            contactUpdateSummary(u.oldContact, u.newContact).isNotEmpty)));
   }
 
   final ContactsRepository contactsRepository;
